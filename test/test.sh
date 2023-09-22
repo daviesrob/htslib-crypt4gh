e1ba07c932c3ea6a4bd3439afacdddaf9d7613d9
Rob Davies <rmd+git@sanger.ac.uk>
2023-09-22 13:43:08 +0100

    Add extra tests, mostly for the htslib interface
    - Test htsFile format auto-detection
    
    - Test htsget, encrypting both the original htsget response and
      the htsget payload

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+pass_count=0
+fail_count=0
+
+show_pass_or_fail () {
+    local res="$1"
+    local name="$2"
+
+    if [ $res -ne 0 ]; then
+        echo "Failed: $name" 1>&2
+        fail_count=$(($fail_count + 1))
+    else
+        echo "Passed: $name" 1>&2
+        pass_count=$(($pass_count + 1))
+    fi
+}
+
 # Find htsfile
 
 echo -n "Looking for htsfile (HTSDIR=$HTSDIR) ... " 1>&2
@@ -29,11 +45,40 @@ export HTS_PATH
 "$HTSFILE" -C test/ce#1.sam crypt4gh:test/ce#1.tmp.encrypted.sam && \
 "$HTSFILE" -C crypt4gh:test/ce#1.tmp.encrypted.sam test/ce#1.tmp.sam && \
 cmp test/ce#1.sam test/ce#1.tmp.sam
+show_pass_or_fail $? 'Round trip test'
 
-if [ $? -ne 0 ] ; then
-    echo "Failed: Round trip test" 1>&2
+# Test auto-detection
+"$HTSFILE" -c test/ce#1.tmp.encrypted.sam > test/ce#1.tmp.2.sam && \
+cmp test/ce#1.sam test/ce#1.tmp.2.sam
+show_pass_or_fail $? 'Auto-detection test'
+
+# Test encrypted htsget
+# Normal htsget input
+"$HTSFILE" -c test/ce#1.htsget > test/ce#1.tmp.3.sam && \
+cmp test/ce#1.sam test/ce#1.tmp.3.sam
+show_pass_or_fail $? 'htsget test'
+
+# Encrypted htsget payload
+"$HTSFILE" -c test/ce#1_encrypted.htsget > test/ce#1.tmp.4.sam && \
+cmp test/ce#1.sam test/ce#1.tmp.4.sam
+show_pass_or_fail $? 'htsget encrypted payload test'
+
+# Encrypted htsget response
+"$HTSFILE" -C test/ce#1.htsget crypt4gh:test/ce#1.tmp.encrypted.htsget && \
+"$HTSFILE" -c test/ce#1.tmp.encrypted.htsget > test/ce#1.tmp.5.sam && \
+cmp test/ce#1.sam test/ce#1.tmp.5.sam
+show_pass_or_fail $? 'htsget encrypted response test'
+
+# Encrypted htsget payload and response
+"$HTSFILE" -C test/ce#1_encrypted.htsget crypt4gh:test/ce#1.tmp.2encrypted.htsget && \
+"$HTSFILE" -c test/ce#1.tmp.2encrypted.htsget > test/ce#1.tmp.6.sam && \
+cmp test/ce#1.sam test/ce#1.tmp.6.sam
+show_pass_or_fail $? 'htsget encrypted payload and response test'
+
+if [ $fail_count -eq 0 ] ; then
+    echo "Passed"
+    exit 0
+else
+    echo "Failed"
     exit 1
 fi
-
-echo "Passed"
-exit 0
